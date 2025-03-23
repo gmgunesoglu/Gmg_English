774a1555e9bf6bb98106041be9c12b5d8b37a0b1
gokhanmuzaffer <gokhangunesoglu@gmail.com>
2025-03-23 21:39:21 +0300

    Redis workout and substructure

--- whisper_model/src/listener.py
+++ whisper_model/src/listener.py
@@ -4,10 +4,14 @@ import numpy as np
 import redis
 import pickle
 from collections import deque
-import threading  # threading modülünü ekliyoruz
+import threading
 
 # Redis bağlantısı
-redis_client = redis.Redis(host='localhost', port=6379, db=0)
+redis_host = "192.168.1.102"
+redis_client = redis.Redis(host=redis_host, port=6379, decode_responses=False)
+
+reading_text = "Helloo my friend, My name's David."
+silence_threshold = 0.01
 
 # Ses kaydı için parametreler
 samplerate = 16000  # 16kHz
@@ -17,23 +21,33 @@ buffer_size = int(samplerate * buffer_duration)
 # Anlık ses kaydı için buffer
 buffer = deque(maxlen=buffer_size)
 
-
 # Ses kaydını almak ve buffer'ı doldurmak için callback fonksiyonu
 def audio_callback(indata, frames, time, status):
     if status:
         print(status, flush=True)
     buffer.extend(indata[:, 0])  # Mono ses
 
-
 # Redis'e veriyi gönderme fonksiyonu
 def send_audio_to_redis():
     while True:
+
+
         if len(buffer) == buffer_size:
+
+            # Sessizlik kontrolü (ortalama mutlak değer)
             audio_data = np.array(buffer, dtype=np.float32)
-            serialized_data = pickle.dumps(audio_data)
-            redis_client.set("audio_buffer", serialized_data)
-            print("Ses Redis'e gönderildi")
-        time.sleep(0.5)  # Her 500ms'de bir güncelle
+            volume_norm = np.linalg.norm(audio_data) / np.sqrt(len(audio_data))
+
+            if volume_norm < silence_threshold:
+                print("Sessizlik algılandı, kuyruğa mesaj eklenmiyor.")
+                time.sleep(0.2)
+            else:
+                serialized_data = pickle.dumps({"message": reading_text, "audio_data": audio_data})
+                redis_client.lpush("reading", serialized_data)
+                print(f"Redis'e gönderildi...\nserialized_data: {serialized_data}")
+                time.sleep(0.1)
+        else:
+            time.sleep(0.5)  # Her 500ms'de bir güncelle
 
 
 # Ses kaydını başlatan fonksiyon
@@ -44,7 +58,6 @@ def start_listener():
         redis_thread = threading.Thread(target=send_audio_to_redis, daemon=True)
         redis_thread.start()
 
-        # Bu blok sürekli çalışacak, ses kaydını sürekli dinleyecek
         while True:
             time.sleep(1)
 
--- whisper_model/src/stt.py
+++ whisper_model/src/stt.py
@@ -5,7 +5,9 @@ import torch
 import time
 
 # Redis bağlantısı
-redis_client = redis.Redis(host='localhost', port=6379, db=0)
+redis_host = "192.168.1.102"
+# redis_client = redis.Redis(host=redis_host, port=6379, db=0)
+redis_client = redis.Redis(host=redis_host, port=6379, decode_responses=False)
 
 # Whisper modelini yükleyin (GPU destekli)
 device = "cuda" if torch.cuda.is_available() else "cpu"
@@ -15,13 +17,15 @@ model = whisper.load_model("large-v3-turbo", device=device)
 # Redis'ten veriyi al ve transkribe et
 def transcribe_from_redis():
     while True:
-        serialized_data = redis_client.get("audio_buffer")
-        if serialized_data:
-            audio_data = pickle.loads(serialized_data)
-            print("Ses verisi alındı, işleniyor...")
-            result = model.transcribe(audio_data, language="en", task="transcribe", temperature=0.5)
-            print("Metin Çıktısı:", result['text'])
-        time.sleep(0.5)  # Her 500ms'de bir kontrol et
+        redis_message = redis_client.rpop("reading")
+        if redis_message:
+            data = pickle.loads(redis_message)
+            audio_data = data["audio_data"]
+            print("Message:", data["message"])
+            result = model.transcribe(audio_data, language="en", task="transcribe", temperature=0.5, initial_prompt=data["message"])
+            print("Model result:", result['text'])
+        else:
+            time.sleep(0.1)
 
 if __name__ == "__main__":
     transcribe_from_redis()
